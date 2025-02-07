79ebf977c7d0c296cf31b6a7dd108b02c19b8a72
anushadkw <anusha.dk@westernbulk.com>
2025-02-07 13:27:02 +0530

    Fetch the genres

--- src/App.tsx
+++ src/App.tsx
@@ -42,6 +42,7 @@ import {
 } from "@chakra-ui/react";
 import NavBar from "./components/NavBar";
 import GameGrid from "./components/GameGrid";
+import GenreList from "./components/GenreList";
 
 function App() {
   return (
@@ -55,7 +56,9 @@ function App() {
         <NavBar></NavBar>
       </GridItem>
       <Show above="lg">
-        <GridItem area={"aside"}>Aside</GridItem>
+        <GridItem area={"aside"}>
+          <GenreList />
+        </GridItem>
       </Show>
       <GridItem area={"main"}>
         <GameGrid />
--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -22,28 +22,6 @@ interface FetchGamesResponse {
 }
 
 const useGames = () =>{
-const [games, setGames] = useState<Game[]>([]);
-  const [error, setError] = useState("");
-  const [isLoading, setLoading] = useState(false);
-
-  useEffect(() => {
-    const controller = new AbortController();
-    setLoading(true);
-    apiClient
-      .get<FetchGamesResponse>("/games", {signal:controller.signal})
-      .then((res) => {
-        setLoading(false);
-        setGames(res.data.results)
-      })
-      .catch((err) => {
-        setLoading(false);
-        if(err instanceof CanceledError)return;
-        setError(err.message);
-      });
-
-      return () => controller.abort();
-  }, []);
-  return {games, error, isLoading};
 }
 
 export default useGames;
